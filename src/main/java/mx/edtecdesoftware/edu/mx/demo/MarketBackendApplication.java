262079a1fdec40a969e0df644fc2014ce6a0cd58
carlosalfonso070717-blip <carlosalfonso070717@gmail.com>
2026-05-12 08:25:23 -0600

    test: adding HelloWordController to test Spring

---
+++ src/main/java/mx/edtecdesoftware/edu/mx/demo/MarketBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.edtecdesoftware.edu.mx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketBackendApplication.class, args);
+    }
+
+}
